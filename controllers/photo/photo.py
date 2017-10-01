09a9d7652581d94733e2a40551e406ac18cdbf2d
sebabouche <sebastien@sifnos.io>
2017-10-01 21:18:03 +0200

    PhotoController class

--- controllers/photo/photo.py
+++ controllers/photo/photo.py
@@ -1,23 +1,39 @@
+import io
+import requests
 from time import sleep
 from datetime import datetime
 from picamera import PiCamera
+from PIL import Image
 
 class PhotoController(object):
     def __init__(self):
         self.camera = PiCamera()
         self.short_id = None
+        self.image = None
 
     def set_short_id(self, short_id):
         self.short_id = short_id
 
     def take_photo(self):
-        if self.short_id:
-            self.camera.capture("/upload/{0}.jpg".format(self.short_id))
-        else:
-            print("Error with short_id")
+        stream = io.BytesIO()
+        self.camera.capture(stream, format="jpeg")
+        stream.seek(0)
+        self.image = Image.open(stream)
 
-    def send_photo(self):
-        pass
+    def process_photo(self, image):
+        box = (418,58, 1502, 1142)
+        region = image.crop(box)
+        polaroid = Image.open("./media/polaroid.jpg")
+        polaroid.paste(region, (50, 50))
+        polaroid.name = self.short_id + ".jpg"
+        return polaroid
+
+    @classmethod
+    def send_photo(cls, image):
+        url = 'https://api.graph.cool/file/v1/cj77htypt0n7g01762fd1hubl'
+        file = {'data': image}
+        requests.post(url, files=file)
+
 
     def delete_photo(self):
         pass
