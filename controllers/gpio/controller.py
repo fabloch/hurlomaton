4602a353b77729fac3a7eb703039446f9f71f914
Raspi Sifnos <raspi@sifnos.io>
2017-10-02 19:36:46 +0200

    Rework at la Fabrique du Loch

--- controllers/gpio/controller.py
+++ controllers/gpio/controller.py
@@ -4,12 +4,16 @@ from RPi import GPIO
 class GPIOController(object):
     def __init__(self):
         self.SOUND_INPUT_PORT = 2
+        self.SPOTS_OUTPUT_PORT = 4
         self.sound_level_high = False
 
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.SOUND_INPUT_PORT, GPIO.IN)
+        GPIO.setup(self.SPOTS_OUTPUT_PORT, GPIO.OUT)
         GPIO.add_event_detect(self.SOUND_INPUT_PORT, GPIO.BOTH)
         GPIO.add_event_callback(self.SOUND_INPUT_PORT, self.set_sound_switch)
+        
+        GPIO.output(self.SPOTS_OUTPUT_PORT, False)
 
     def set_sound_switch(self, *args, **kwargs):
         """
@@ -26,12 +30,15 @@ class GPIOController(object):
         """
         # print("sound test running")
         score = 0
-        for _ in range(0, 4):
+        for _ in range(0, 3):
             if self.sound_level_high:
                 score += 1
             sleep(1/4)
             print("Score {0} at {1}".format(score, time()))
-        if score == 4:
+        if score == 3:
             return True
         else:
             return False
+
+    def spots_on(self, switch = False):
+        GPIO.output(self.SPOTS_OUTPUT_PORT, switch)
