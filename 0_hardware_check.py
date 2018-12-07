adba2ae51f8b127a3bc6e93ba3559c9f1d91d5e2
taewyth <lucas.doula@gmail.com>
2018-12-07 11:19:19 +0100

    Stuff didn't work

--- 0_hardware_check.py
+++ 0_hardware_check.py
@@ -17,7 +17,7 @@ whiteTest = False
 
 #Test du static
 print("\033[1;36;40m Verification de l'électricité statique...")
-if GPIO.input(myGPIO.sound_level_high) == True:
+if GPIO.input(myGPIO.SOUND_INPUT_PORT) == 1:
     print("\033[1;31;40m problème d'électricité statique\n")
     print("\033[1;36;40m débranchez la machine quelques instants")
     sleep(1)
@@ -27,9 +27,9 @@ sleep(1)
 #Test du micro
 print("\033[1;36;40m Verification du micro...")
 print("\033[1;36;40m Hurlez s'il vous plaît")
-print(myGPIO.sound_level_high)
+print(myGPIO.SOUND_INPUT_PORT))
 while mikeTest == False:
-    if GPIO.input(myGPIO.sound_level_high) == True:
+    if GPIO.input(myGPIO.SOUND_INPUT_PORT) == 1:
         mikeTest == True
 print("\033[1;32;40m OK  \n")
 sleep(1)
