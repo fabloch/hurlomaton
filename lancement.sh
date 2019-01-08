bb1017dc40c6fce3e33480300bf671b29c7ecbaa
taewyth <lucas.doula@gmail.com>
2019-01-08 17:46:08 +0100

    a

--- lancement.sh
+++ lancement.sh
@@ -3,6 +3,6 @@ source /home/pi/dev/virtualenv/hurlo/bin/activate
 cd /home/pi/dev/hurlomaton
 python3 0_hardware_check.py &&
 hardware = $!
-wait[$!] &
+wait[hardware] &
 python3 1_crop_watch.py &
 python3 3_hurlomaton.py
