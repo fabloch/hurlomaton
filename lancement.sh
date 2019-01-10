193051815db72854690527af01e9155cd1987f8c
taewyth <lucas.doula@gmail.com>
2019-01-10 10:37:37 +0100

    Now trying to find the source of a PEBKAC type error

--- lancement.sh
+++ lancement.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 source /home/pi/dev/virtualenv/hurlo/bin/activate
 cd /home/pi/dev/hurlomaton
-python3 0_hardware_check.py &&
-hardware = $!
-wait[hardware] &
-python3 1_crop_watch.py &
-python3 3_hurlomaton.py
